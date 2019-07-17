b4e852578b538087c13652bf5dbe09159f5484df
ivanovip1934 <ivanovip1934@outlook.com>
2019-07-17 14:24:13 +0300

    Добавьте файлы проекта.

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double val = 8.0;
+            string str = "Alpha Beta Gamma";
+
+            Console.WriteLine($"Обратная величина {val} равна {val.Reciprocal()}");
+
+            Console.WriteLine($"{str} после смены регистра: {str.RevCase()}");
+
+            Console.WriteLine($"Результат вызова метода val.AbsDivideBy(-2): {val.AbsDidideBy(-2)}");
+
+            Console.ReadKey();
+        }
+    }
+}
